845b0f675f3efc0825b9d87ac4f4a07aa70f1611
l11040 <l1104033837@gmail.com>
2025-07-06 16:23:24 +0900

    [Add] 식당 모듈 및 관련 컨트롤러, 서비스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { typeOrmConfig } from './utils/typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmConfig),
+    RestaurantModule,
   ],
   controllers: [AppController],
   providers: [AppService],
