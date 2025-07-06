a5075df420f2ffe85cb7a0e88941bfb8ac60cb0e
l11040 <l1104033837@gmail.com>
2025-07-07 01:40:33 +0900

    [Update] 식당 모듈에 인증 모듈 및 엔티티 추가

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantController } from './restaurant.controller';
+import { RestaurantAuthModule } from './auth/auth.module';
+import { Restaurant } from 'src/entities/restaurant.entity';
+import { Menu } from 'src/entities/menu.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Restaurant, Menu]), RestaurantAuthModule],
   providers: [RestaurantService],
   controllers: [RestaurantController],
+  exports: [RestaurantAuthModule],
 })
 export class RestaurantModule {}
